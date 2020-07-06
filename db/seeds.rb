0ef495d6cf3fa16a46275f091f8e1caf132408da
junichi fujita <fujita0909j@gmail.com>
2020-07-06 17:28:28 +0900

    [add] recruitmentモデルのseedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env == "development"
+  10.times do |i|
+    Recruitment.create!(
+      title: "test#{i}",
+      content: "test_content#{i}",
+      appropriate_level: "#{i}",
+      start_time: "2020-07-6",
+      play_time: "#{i}",
+      master_name: "master#{i}",
+    )
+  end
+end
