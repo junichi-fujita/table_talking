eed0a37e29674eea36f220d91f17dff70f43905b
junichi fujita <fujita0909j@gmail.com>
2020-07-07 23:00:20 +0900

    [add] recruitmentの新規作成画面の作成

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -1,5 +1,5 @@
 class RecruitmentsController < ApplicationController
-  before_action :set_recruitment, only: %i[show create edit update destroy]
+  before_action :set_recruitment, only: %i[show edit update destroy]
   def index
     @recruitments = Recruitment.order(id: :desc)
   end
@@ -9,11 +9,18 @@ class RecruitmentsController < ApplicationController
   end
 
   def new
-
+    @recruitment = Recruitment.new
   end
 
   def create
-
+    @recruitment = Recruitment.new(recruitment_paramas)
+    # master_nameをcurrent_userに変更する予定
+    @recruitment.assign_attributes(master_name: "yamada")
+    if @recruitment.save
+      redirect_to @recruitment, notice: "募集内容を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -32,5 +39,16 @@ class RecruitmentsController < ApplicationController
 
   def set_recruitment
     @recruitment = Recruitment.find(params[:id])
+  end
+
+  def recruitment_paramas
+    params.require(:recruitment).permit(
+      :title,
+      :content,
+      :appropriate_level,
+      :master_name,
+      :start_time,
+      :play_time,
+    )
   end
 end
