0bcd3d1edc4252e4e0aeb1c5d2293530578fa000
junichi fujita <fujita0909j@gmail.com>
2020-07-27 19:29:23 +0900

    [fix] rspecのエラーを修正

--- spec/factories/participant_management.rb
+++ spec/factories/participant_management.rb
@@ -1,12 +1,10 @@
 FactoryBot.define do
   factory :participant_management do
-    # user_id { User.first.id }
-    # recruitment_id { Recruitment.first.id }
     user
     recruitment
   end
-  # factory :second_participant_management, class: ParticipantManagement do
-  #   user_id { User.second.id }
-  #   recruitment_id { Recruitment.second.id}
-  # end
+  factory :second_participant_management, class: ParticipantManagement do
+    second_user
+    second_recruitment
+  end
 end
